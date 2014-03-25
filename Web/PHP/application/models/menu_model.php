39d5fdb7222b693e0467d8e9102ece6ee4309728
linjiang9999@gmail.com <linjiang9999@gmail.com>
2014-03-26 01:21:09 +0800

    bug

--- Web/PHP/application/models/menu_model.php
+++ Web/PHP/application/models/menu_model.php
@@ -11,6 +11,7 @@ class Menu_model extends CI_Model
 	
 	public function get_menu()
 	{
+		$this->db->order_by('id', 'asc');
 		$query = $this->db->get_where(CH_TABLE_MENU, array('tid' => self::$default_menu_type));
 		return $query->result();
 	}
